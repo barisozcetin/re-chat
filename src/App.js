ff0cc6eb1488efe018e17eb0038c05dbc1b8595c
baris_000 <barisozcetin@gmail.com>
2018-07-21 18:22:21 -0700

    nickname field added to main provider

--- src/App.js
+++ src/App.js
@@ -1,75 +1,75 @@
-import React, { Component } from "react";
-import Router from "./Router";
-import { firebaseApp } from "./base";
-import "./App.css";
+import React, { Component } from 'react';
+import Router from './Router';
+import { firebaseApp } from './base';
+import './App.css';
 
 const MainContext = React.createContext();
 
 class MainProvider extends Component {
-  state = {
-    isAuthenticated: false,
-    user: "Anonymous"
-  };
+	state = {
+		isAuthenticated: false,
+		user: 'Anonymous',
+		nickname: 'Anonymous'
+	};
 
-  onLogin = user => {
-    this.setState({ isAuthenticated: true, user });
-  };
+	onLogin = (user) => {
+		this.setState({ isAuthenticated: true, user, nickname: 'demo' });
+	};
 
-  signOut = () => {
-    firebaseApp
-      .auth()
-      .signOut()
-      .then(() =>
-        this.setState({
-          isAuthenticated: false,
-          user: "Anonymous"
-        })
-      );
-  };
+	signOut = () => {
+		firebaseApp.auth().signOut().then(() =>
+			this.setState({
+				isAuthenticated: false,
+				user: 'Anonymous'
+			})
+		);
+	};
 
-  componentDidMount() {
-    this.removeListener = firebaseApp.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.setState({
-          isAuthenticated: true,
-          user: user.email
-        });
-      } else {
-        this.setState({
-          isAuthenticated: false,
-          user: ""
-        });
-      }
-    });
-  }
+	componentDidMount() {
+		this.removeListener = firebaseApp.auth().onAuthStateChanged((user) => {
+			if (user) {
+				this.setState({
+					isAuthenticated: true,
+					user: user.email,
+					nickname: user.email
+				});
+			} else {
+				this.setState({
+					isAuthenticated: false,
+					user: '',
+					nickname: ''
+				});
+			}
+		});
+	}
 
-  render() {
-    return (
-      <MainContext.Provider
-        value={{
-          state: this.state,
-          onLogin: this.onLogin,
-          signOut: this.signOut
-        }}
-      >
-        {this.props.children}
-      </MainContext.Provider>
-    );
-  }
+	render() {
+		return (
+			<MainContext.Provider
+				value={{
+					state: this.state,
+					onLogin: this.onLogin,
+					signOut: this.signOut
+				}}
+			>
+				{this.props.children}
+			</MainContext.Provider>
+		);
+	}
 }
 
 export { MainContext };
 
 class App extends Component {
-  render() {
-    return (
-      <MainProvider>
-        <div id="app">
-          <Router />
-        </div>
-      </MainProvider>
-    );
-  }
+	render() {
+		return (
+			<MainProvider>
+				<div id="app">
+					<Router />
+				</div>
+			</MainProvider>
+		);
+	}
 }
 
 export default App;
