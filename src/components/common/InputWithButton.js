d15e18968c13792e2e26e8bb85c26554b66b47c4
baris_000 <barisozcetin@gmail.com>
2018-07-21 22:07:30 -0700

    input with button input length control added

--- src/components/common/InputWithButton.js
+++ src/components/common/InputWithButton.js
@@ -1,46 +1,46 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const InputWithButton = ({
-  type = "string",
-  name,
-  value = "",
-  placeholder,
-  buttonText,
-  onChange,
-  onClick,
-  readOnly = false,
-  extraClass = ""
+	type = 'string',
+	name,
+	value = '',
+	placeholder,
+	buttonText,
+	onChange,
+	onClick,
+	readOnly = false,
+	extraClass = ''
 }) => {
-  return (
-    <div className={`field has-addons ${extraClass}`}>
-      <div className="control is-expanded">
-        <input
-          className="input"
-          type={type}
-          name={name}
-          onChange={onChange}
-          placeholder={placeholder}
-          value={value}
-          readOnly={readOnly}
-        />
-      </div>
-      <div className="control">
-        <button className="button is-info" onClick={onClick}>
-          {buttonText}
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className={`field has-addons ${extraClass}`}>
+			<div className="control is-expanded">
+				<input
+					className="input"
+					type={type}
+					name={name}
+					onChange={onChange}
+					placeholder={placeholder}
+					value={value}
+					readOnly={readOnly}
+				/>
+			</div>
+			<div className="control">
+				<button className="button is-info" disabled={value.trim().length === 0} onClick={onClick}>
+					{buttonText}
+				</button>
+			</div>
+		</div>
+	);
 };
 
 InputWithButton.propTypes = {
-  type: PropTypes.string,
-  name: PropTypes.string.isRequired,
-  placeholder: PropTypes.string,
-  buttonText: PropTypes.string,
-  value: PropTypes.string,
-  onChange: PropTypes.func
+	type: PropTypes.string,
+	name: PropTypes.string.isRequired,
+	placeholder: PropTypes.string,
+	buttonText: PropTypes.string,
+	value: PropTypes.string,
+	onChange: PropTypes.func
 };
 
 export default InputWithButton;
